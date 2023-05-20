811e32ae9c8085e4706c5d78c0689becfada01fb
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-05-20 17:24:15 -0300

    created thought model

---
+++ models/Thought.js
@@ -0,0 +1,63 @@
+const { Schema, Types, model } = require("mongoose");
+const DayJS = require("dayjs");
+
+const dayjs = new DayJS();
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: formatDate,
+  },
+});
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      minLength: 1,
+      maxLength: 280,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJson: {
+      virtual: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+function formatDate(createdAt) {
+  return dayjs(createdAt).formatDate("MMMM D, YYYY");
+}
+
+const Thought = model("thought", userSchema);
+
+module.exports = Thought;
