23b41e514350ec98320cb5565e9ff25861fb7a76
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-05-20 20:59:08 -0300

    Added user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,2 +1,17 @@
+const {
+  getUsers,
+  createUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+} = require("../../controllers/userController");
+
 const router = require("express").Router();
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// api/users/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
 module.exports = router;
